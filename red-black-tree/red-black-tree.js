2e9c7e85e2ebc51f59344bc6ddb10623467a186e
hard <453824417@qq.com>
2016-07-18 18:44:36 +0800

    red-black-tree delete

--- red-black-tree/red-black-tree.js
+++ red-black-tree/red-black-tree.js
@@ -14,9 +14,6 @@ function RedBlackNode(key,color) {
     this.rightSon = null;
 }
 
-RedBlackNode.prototype.RED = false;
-
-RedBlackNode.prototype.BLACK = true;
 
 RedBlackNode.prototype.changeColor = function() {
     this.color = !this.color;
@@ -69,6 +66,11 @@ RedBlackNode.prototype.uncleColor = function () {
         return this.leftColor();
     }
 }
+
+
+RedBlackTree.prototype.RED = false;
+
+RedBlackTree.prototype.BLACK = true;
 
 RedBlackTree.prototype.rightRotate = function (now) {
     var father = now.father;
@@ -142,11 +144,11 @@ RedBlackTree.prototype.rotate = function (now) {
 
 RedBlackTree.prototype.insAjust = function (now) {
     //case 1, father is black,over
-    if(now.fatherColor()==now.BLACK){
+    if(now.fatherColor()==this.BLACK){
         return ;
     }
 
-    if(now.uncleColor()==now.RED){
+    if(now.uncleColor()==this.RED){
         var grandFather = now.father.father;
         grandFather.changeColor();
         grandFather.leftSon.changeColor();
@@ -177,7 +179,7 @@ RedBlackTree.prototype.insert = function(key) {
     //dup key ,insert fail
     if(now.key!=null)return false;
     now.key = key;
-    now.color = now.RED;
+    now.color = this.RED;
     now.leftLink(new RedBlackNode(null,true));
     now.rightLink(new RedBlackNode(null,true));
     this.insAjust(now);
@@ -185,9 +187,164 @@ RedBlackTree.prototype.insert = function(key) {
 }
 
 
+RedBlackTree.prototype.find = function (key) {
+    var now = this.root;
+    while(now.key != null){
+        if(key < now.key){
+            now = now.leftSon;
+        }else if(key > now.key){
+            now = now.rightSon;
+        }else{
+            break;
+        }
+    }
+    if(now.key == key)return now;
+    else return null;
+}
 
+RedBlackTree.prototype.delAjust = function (node) {
+    if(node == null || node.color == this.BLACK || node.father == null){
+        return ;
+    }
+    var father = node.father;
+    if(father.leftSon == node){
+        var brother = father.rightSon;
+        if(brother.color == this.RED){
+            father.changeColor();
+            brother.changeColor();
+            var gFather = father.father;
+            var newNode = this.leftRotate(brother);
+            if(gFather==null){
+                this.root = newNode
+            }else{
+                if(gFather.leftSon == father){
+                    gFather.leftLink(newNode);
+                }else{
+                    gFather.rightLink(newNode);
+                }
+            }
+            this.delAjust(node);
+        }else{
+            if(brother.leftColor()==this.BLACK && brother.rightColor()==this.BLACK){
+                brother.changeColor();
+                this.delAjust(father);
+            }else if(brother.leftColor()==this.RED && brother.rightColor()==this.BLACK){
+                brother.changeColor();
+                brother.leftSon.changeColor();
+                father.rightLink(this.rightRotate(brother.leftSon));
+                this.delAjust(node);
+            }else if(brother.rightColor()==this.BLACK){
+                brother.color = father.color;
+                father.color = this.BLACK;
+                brother.rightSon.color = this.BLACK;
+                var gFather = father.father;
+                var newNode = this.leftRotate(brother);
+                if(gFather==null){
+                    this.root = newNode;
+                    this.root.color = this.BLACK;
+                    this.root.father = null;
+                }else{
+                    if(gFather.leftSon == father){
+                        gFather.leftLink(newNode);
+                    }else{
+                        gFather.rightLink(newNode);
+                    }
+                }
+                this.delAjust(gFather);
+            }
+        }
+    }else{
+        var brother = father.leftSon;
+        if(brother.color == this.RED){
+            father.changeColor();
+            brother.changeColor();
+            var gFather = father.father;
+            var newNode = this.rightRotate(brother);
+            if(gFather==null){
+                this.root = newNode
+            }else{
+                if(gFather.leftSon == father){
+                    gFather.leftLink(newNode);
+                }else{
+                    gFather.rightLink(newNode);
+                }
+            }
+            this.delAjust(node);
+        }else{
+            if(brother.leftColor()==this.BLACK && brother.rightColor()==this.BLACK){
+                brother.changeColor();
+                this.delAjust(father);
+            }else if(brother.leftColor()==this.BLACK && brother.rightColor()==this.RED){
+                brother.changeColor();
+                brother.rightSon.changeColor();
+                father.leftLink(this.leftRotate(brother.rightSon));
+                this.delAjust(node);
+            }else if(brother.leftColor()==this.BLACK){
+                brother.color = father.color;
+                father.color = this.BLACK;
+                brother.leftSon.color = this.BLACK;
+                var gFather = father.father;
+                var newNode = this.rightRotate(brother);
+                if(gFather==null){
+                    this.root = newNode;
+                    this.root.color = this.BLACK;
+                    this.root.father = null;
+                }else{
+                    if(gFather.leftSon == father){
+                        gFather.leftLink(newNode);
+                    }else{
+                        gFather.rightLink(newNode);
+                    }
+                }
+                this.delAjust(gFather);
+            }
+        }
+    }
+}
 
+RedBlackTree.prototype.delete = function (key) {
+    var node = this.find(key);
+    if(node == null)return false;
+    //has two child
+    var old = node, child = null, father = null;
+    if(node.leftKey()!=null && node.rightKey()!=null){
+        node = node.rightSon;
+        while(node.leftKey()!=null){
+            node = node.leftSon;
+        }
+        child = node.rightSon;
+        father = node.father;
+        if(father.leftSon==node){
+            father.leftLink(child);
+        }else{
+            father.rightLink(child);
+        }
+        old.key = node.key;
+    }else{
+        if(node.leftKey() != null){
+            child = node.leftSon;
+        }else{
+            child = node.rightSon;
+        }
+        father = node.father;
+        if(father==null){
+            this.root = node;
+            this.root.father = null;
+            this.root.color = this.BLACK;
+        }else{
+            if(father.leftSon==node){
+                father.leftLink(child);
+            }else{
+                father.rightLink(child);
+            }
+        }
+    }
+    if(node.color == this.BLACK){
+        this.delAjust(node);
+    }
+    return true;
 
+}
 
 var main = function () {
     var tree = new RedBlackTree();
@@ -197,6 +354,10 @@ var main = function () {
     tree.insert(6);
     tree.insert(100);
     tree.insert(29);
+    tree.delete(100);
+    tree.delete(2);
+    tree.delete(3);
+    tree.delete(29);
     console.log(tree.root);
 }();
 
